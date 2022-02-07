2a92307e60bb9f3ccb4372e7ad9330b622255e41
pumbf <wpn892862286@gmail.com>
2022-02-07 16:48:48 +0800

    fix:使用junit5, 修复junit4依赖不存在的问题

--- src/test/java/cn/monitor4all/logRecord/test/operationLogNameTest/LogRecordFuncTest.java
+++ src/test/java/cn/monitor4all/logRecord/test/operationLogNameTest/LogRecordFuncTest.java
@@ -1,30 +1,28 @@
 package cn.monitor4all.logRecord.test.operationLogNameTest;
 
-import cn.monitor4all.logRecord.annotation.OperationLog;
 import cn.monitor4all.logRecord.bean.LogDTO;
 import cn.monitor4all.logRecord.configuration.LogRecordAutoConfiguration;
 import cn.monitor4all.logRecord.function.LogRecordFunc;
 import cn.monitor4all.logRecord.service.CustomLogListener;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestComponent;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * @author pumbf
  * @version 1.0
  * @since 2022-01-28 21:16
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ContextConfiguration(classes = {LogRecordAutoConfiguration.class, LogRecordFuncTest.TestLogListener.class, OperationLogService.class})
 @EnableAspectJAutoProxy(proxyTargetClass = true)
@@ -46,7 +44,7 @@ public class LogRecordFuncTest {
         @Override
         public void createLog(LogDTO logDTO) throws Exception {
             log.info(JSON.toJSONString(logDTO));
-            Assert.assertEquals(logDTO.getBizId(),  "test");
+            Assertions.assertEquals(logDTO.getBizId(),  "test");
         }
     }
 
